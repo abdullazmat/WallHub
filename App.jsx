6c7ef08f270566ba570c2a72b4a23ba1c48f18eb
Abdullah <shinsukenakamurahard@gmail.com>
2025-03-19 18:56:01 +0500

    React Native Icon Setup

--- App.jsx
+++ App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 import {StatusBar} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.contianer}>
       <StatusBar barStyle="dark-content" />
+      <Text>Helo</Text>
+      <AntDesign name={'right'} size={40} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contianer: {
+    paddingTop: 40,
+    paddingStart: 30,
+  },
+});
 
 export default App;
