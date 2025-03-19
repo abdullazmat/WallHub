0216962550d370732b8176854d0b750bc4a8fcd3
Abdullah <shinsukenakamurahard@gmail.com>
2025-03-19 18:02:25 +0500

    WallHub Project Initialized

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+import {StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
